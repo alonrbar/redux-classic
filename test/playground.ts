98bd4af225790888e290d68d601ee1d66e757101
Alon Bar <alon.r.bar@gmail.com>
2017-11-29 14:20:33 +0200

    playground reset

--- test/playground.ts
+++ test/playground.ts
@@ -1,49 +1,11 @@
 import { expect } from 'chai';
-import { component, ReduxApp } from 'src';
-import { Component } from 'src/components';
+
+// tslint:disable:no-unused-expression
 
 describe('playground', () => {
-    it("a place to run wild", () => {
+    it("a place to run wild", () => {        
 
-        @component
-        class Root {
-            public first = {
-                second: new Level2()
-            };
-        }
-
-        class Level2 {
-            public theComponent = new ThisIsAComponent();
-        }
-
-        @component
-        class ThisIsAComponent {
-
-            public value: string = 'before';
-
-            public changeValue() {
-                this.value = 'after';
-            }
-        }
-
-        const preLoadedState = {
-            first: {
-                second: {
-                    theComponent: {
-                        value: 'I am here!'
-                    }
-                }
-            }
-        };
-
-        // create component tree
-        const root = new Root();
-        expect(root.first.second.theComponent).to.be.an.instanceOf(ThisIsAComponent);
-        expect(root.first.second.theComponent.value).to.eql('before');
-
-        // create the app
-        const app = new ReduxApp(root, undefined, preLoadedState);
-        expect(app.root.first.second.theComponent).to.be.an.instanceOf(Component);
-        expect(app.root.first.second.theComponent.value).to.eql('I am here!');
+        // right your test here...
+        expect(true).to.be.true;
     });
 });
